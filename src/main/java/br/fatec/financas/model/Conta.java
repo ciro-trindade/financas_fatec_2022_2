2d5bceb0d771a8ec4591e648d729ed80fbd5e633
Ciro Cirne Trindade <ciroct@gmail.com>
2022-08-25 21:56:59 -0300

    Primeiro commit

---
+++ src/main/java/br/fatec/financas/model/Conta.java
@@ -0,0 +1,99 @@
+package br.fatec.financas.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "tb_conta")
+@Entity
+public class Conta implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nr_agencia", nullable = false)
+	private Integer agencia;
+	
+	@Column(name = "nm_numero", length = 15)
+	private String numero;
+	
+	@Column(name = "nm_titular", length = 100)	
+	private String titular;
+	
+	@Column(name = "vl_saldo")
+	private Float saldo;
+	
+	public Conta() {
+	}
+
+	public Conta(Long id) {
+		this.id = id;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(Integer agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public Float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Float saldo) {
+		this.saldo = saldo;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		return Objects.equals(id, other.id);
+	}
+			
+
+}
