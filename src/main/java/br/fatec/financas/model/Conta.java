9d3d989b66921a0856c2de2fe6afa7f8be4f7c50
Ciro Cirne Trindade <ciroct@gmail.com>
2022-09-01 21:52:54 -0300

    Heranca

--- src/main/java/br/fatec/financas/model/Conta.java
+++ src/main/java/br/fatec/financas/model/Conta.java
@@ -1,23 +1,13 @@
 package br.fatec.financas.model;
 
-import java.io.Serializable;
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table(name = "tb_conta")
 @Entity
-public class Conta implements Serializable {
+public class Conta extends AbstractEntity {
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	
 	@Column(name = "nr_agencia", nullable = false)
 	private Integer agencia;
@@ -32,18 +22,6 @@ public class Conta implements Serializable {
 	private Float saldo;
 	
 	public Conta() {
-	}
-
-	public Conta(Long id) {
-		this.id = id;
-	}
-	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	public Integer getAgencia() {
@@ -76,24 +54,6 @@ public class Conta implements Serializable {
 
 	public void setSaldo(Float saldo) {
 		this.saldo = saldo;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Conta other = (Conta) obj;
-		return Objects.equals(id, other.id);
-	}
-			
+	}			
 
 }
